34aec02341b733564fdb324f09b47c31e6899d30
Jonas Timmermann <ga56gof@mytum.de>
2016-04-25 12:26:58 -0700

    Added tests: encode strings as numbers

--- notebooks/test_election2016.py
+++ notebooks/test_election2016.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from election import Election2016
+import numpy as np
 
 
 class TestElection2016(TestCase):
@@ -24,6 +25,22 @@ class TestElection2016(TestCase):
         self.assertTrue(len(e.pr[e.pr['party'] == 'Democrat']) == 0)
 
         print 'Democrats have successfully been removed from the data-set'
+
+    def test_preprocessingStringEncoding(self):
+        e = Election2016()
+        e.loadDataSets()
+
+        self.assertTrue(type(e.pr['state_abbreviation'].iloc[0]) is str)
+
+        e.preprocessDataSets()
+
+        self.assertFalse('state_abbreviation' in e.data.columns)
+        self.assertTrue('state_number' in e.data.columns)
+
+        self.assertTrue(type(e.data['state_number'].iloc[0]) is np.int64)
+
+        print 'Srings have been encoded as numbers for regression model'
+
 
 
 
