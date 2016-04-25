c1a4b19c0a7b2ef283a329c4e27cc2ac2133c9b3
Jonas Timmermann <ga56gof@mytum.de>
2016-04-25 12:13:45 -0700

    Added tests: length of dataset, democrats removed

--- notebooks/test_election2016.py
+++ notebooks/test_election2016.py
@@ -6,8 +6,24 @@ class TestElection2016(TestCase):
     def test_loadDataSets(self):
         election = Election2016()
         election.loadDataSets()
-        self.assertFalse(election.cf.empty, "Loading county facts")
-        self.assertFalse(election.pr.empty, "Loading prediction results")
+        self.assertFalse(election.cf.empty)
+        self.assertFalse(election.pr.empty)
+
+        self.assertTrue(len(election.cf.columns) == 54)
+        self.assertTrue(len(election.pr.columns) == 8)
+        print 'Data sets have been loaded correctly'
+
+    def test_preprocessingRemoveDemocrats(self):
+        e = Election2016()
+        e.loadDataSets()
+
+        self.assertTrue(len(e.pr[e.pr['party'] == 'Democrat']) > 0)
+
+        e.preprocessDataSets()
+
+        self.assertTrue(len(e.pr[e.pr['party'] == 'Democrat']) == 0)
+
+        print 'Democrats have successfully been removed from the data-set'
 
 
 
